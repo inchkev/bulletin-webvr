c5ee652a09276a2e37d9ad91051b789ca07ea480
Kevin Chen <k3vnchen@gmail.com>
2019-10-26 10:18:46 -0400

    Allow dynamic bulletin and bulletin text creation

--- components/bulletin-text.js
+++ components/bulletin-text.js
@@ -21,7 +21,7 @@ AFRAME.registerComponent('bulletin-text', {
     },
 
     onTextPlaced: function() {
-      console.log('detected enter');
+      // console.log('detected enter');
       
       var cam = document.getElementById('cam');
       var position = cam.object3D.position;
@@ -29,9 +29,9 @@ AFRAME.registerComponent('bulletin-text', {
       var x = rotation.x;
       var y = rotation.y;
 
-      console.log(position);
-      console.log(x);
-      console.log(y);
+      // console.log(position);
+      // console.log(x);
+      // console.log(y);
       var text = "If you are working on something that you really care about, you don’t have to be pushed. The vision pulls you."
       var id = 1234;
       this.addTextWithID(x, y, text, 2.5, 2.5, 2, id);
@@ -47,7 +47,7 @@ AFRAME.registerComponent('bulletin-text', {
       var rot = y * (180 / Math.PI);
       if (ypos < mid - h/2 || ypos > mid + h/2) {
         console.log('Text placement out of bounds');
-        return;
+        return 1;
       }
 
       this.removeTextWithID(1234);
@@ -57,7 +57,7 @@ AFRAME.registerComponent('bulletin-text', {
       text.setAttribute('id', id);
       text.setAttribute('position', xpos.toString() + ' ' + ypos.toString() + ' ' + zpos.toString());
       text.setAttribute('rotation', '0 ' + rot.toString() + ' 0');
-      var textvalue = 'color: white; align: center; font: sourcecodepro; width: 1.25; value: ' + str;
+      var textvalue = 'color: white; align: center; width: 1.25; value: ' + str;
       text.setAttribute('text', textvalue);
       text.setAttribute('font', 'sourcecodepro');
       text.setAttribute('wrap-count', 25);
@@ -71,7 +71,9 @@ AFRAME.registerComponent('bulletin-text', {
     removeTextWithID: function(id) {
       var elem = document.getElementById(id);
       var scene = document.querySelector('a-scene');
-      if (elem != null) {
+      if (elem == null) {
+        return 1;
+      } else {
         return scene.removeChild(elem);
       }
     }
--- components/bulletin.js
+++ components/bulletin.js
@@ -1,19 +1,42 @@
 AFRAME.registerComponent('bulletin', {
   
     init: function () {
-      // poll database of messages
-
+      this.bindMethods();
       this.el.addEventListener('loaded', this.load.bind(this));
     },
     
     load: function() {
       var scene = document.querySelector('a-scene');
+      this.initBulletin(3, 3, 2, 'wellness');
+    },
+
+    bindMethods: function() {
+      this.initBulletin = this.initBulletin.bind(this);
+    },
+
+    initBulletin: function(h, r, mid, text) {
+      var scene = document.querySelector('a-scene');
 
       var board = document.createElement('a-entity');
-      board.setAttribute('geometry', 'primitive: cylinder; height: 3; radius: 3; open-ended: true');
+      board.setAttribute('geometry', 'primitive: cylinder; height: ' + h + '; radius: ' + r + '; open-ended: true');
       board.setAttribute('material', 'color: #F7B860; opacity: 0.7; side: double');
-      board.setAttribute('position', '0 2 0');
-
+      board.setAttribute('position', '0 ' + mid + ' 0');
       scene.appendChild(board);
+
+      var banner = document.createElement('a-text');
+      banner.setAttribute('id', 'bulletin-banner');
+      banner.setAttribute('geometry', 'primitive: plane; height: auto; width: 3.75');
+      banner.setAttribute('material', 'color: #FFFFFF');
+      
+      var y = mid + h/2 - 0.5;
+      var z = -r + 0.75;
+      var str = 'Welcome to the ' + text + ' bulletin!\nPlease be mindful of what you post.'
+      banner.setAttribute('position', '0 ' + y.toString() + ' ' + z.toString());
+      
+      var bannervalue = 'color: white; baseline: center; align: center; width: 4.5; value: ' + str;
+      banner.setAttribute('text', bannervalue);
+      banner.setAttribute('font', 'roboto');
+      banner.setAttribute('wrap-count', 38);
+      scene.appendChild(banner);
     }
   });
