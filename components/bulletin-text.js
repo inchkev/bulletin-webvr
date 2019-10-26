04c66bbddeeb4d674c6c4b6051f6bcf112f06424
Kevin Chen <k3vnchen@gmail.com>
2019-10-26 09:25:44 -0400

    Tweak text settings

--- components/bulletin-text.js
+++ components/bulletin-text.js
@@ -31,26 +31,31 @@ AFRAME.registerComponent('bulletin-text', {
       console.log(position);
       console.log(x);
       console.log(y);
-      var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      this.placeText(x, y, text, 2.5, 3, 2);
+      var text = "If you are working on something that you really care about, you don’t have to be pushed. The vision pulls you."
+      this.placeText(x, y, text, 2.5, 2.5, 2);
     },
 
     placeText: function(x, y, str, r, h, mid) {
       var camheight = 1.6;
       var scene = document.querySelector('a-scene');
 
-      var text = document.createElement('a-text');
       var xpos = -r * Math.sin(y);
       var ypos = r * Math.tan(x) + camheight;
       var zpos = -r * Math.cos(y);
       var rot = y * (180 / Math.PI);
-      console.log(xpos);
-      console.log(ypos);
-      console.log(zpos);
+      if (ypos < mid - h/2 || ypos > mid + h/2) {
+        console.log('Text placement out of bounds');
+        return;
+      }
+
+      // console.log(xpos);
+      // console.log(ypos);
+      // console.log(zpos);
+      var text = document.createElement('a-text');
       
       text.setAttribute('position', xpos.toString() + ' ' + ypos.toString() + ' ' + zpos.toString());
       text.setAttribute('rotation', '0 ' + rot.toString() + ' 0');
-      var textvalue = 'color: white; align: center; font: sourcecodepro; width: 1; value: ' + str;
+      var textvalue = 'color: white; align: center; font: sourcecodepro; width: 1.25; value: ' + str;
       text.setAttribute('text', textvalue);
       text.setAttribute('font', 'sourcecodepro');
       text.setAttribute('wrap-count', 25);
