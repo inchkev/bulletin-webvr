2779ba4b79d6dbd7bdce53ee825a43fcb582b479
Kevin Chen <k3vnchen@gmail.com>
2019-10-26 09:15:52 -0400

    Add text and autorotation

--- components/bulletin-text.js
+++ components/bulletin-text.js
@@ -31,29 +31,33 @@ AFRAME.registerComponent('bulletin-text', {
       console.log(position);
       console.log(x);
       console.log(y);
-
-      this.placeText(x, y, "Hello, World!");
+      var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      this.placeText(x, y, text, 2.5, 3, 2);
     },
 
-    placeText: function(x, y, str) {
-      var r = 3;
-      var h = 3;
-      var mid = 2;
-      var cam = 1.6;
+    placeText: function(x, y, str, r, h, mid) {
+      var camheight = 1.6;
       var scene = document.querySelector('a-scene');
 
-      var text = document.createElement('a-entity');
+      var text = document.createElement('a-text');
       var xpos = -r * Math.sin(y);
-      var ypos = r * Math.tan(x) + cam;
+      var ypos = r * Math.tan(x) + camheight;
       var zpos = -r * Math.cos(y);
+      var rot = y * (180 / Math.PI);
       console.log(xpos);
       console.log(ypos);
       console.log(zpos);
+      
       text.setAttribute('position', xpos.toString() + ' ' + ypos.toString() + ' ' + zpos.toString());
-      text.setAttribute('geometry', 'primitive: box; acolor: red');
-      // text.setAttribute('text', 'value: ' + str + '; width: 40; color: black');
+      text.setAttribute('rotation', '0 ' + rot.toString() + ' 0');
+      var textvalue = 'color: white; align: center; font: sourcecodepro; width: 1; value: ' + str;
+      text.setAttribute('text', textvalue);
+      text.setAttribute('font', 'sourcecodepro');
+      text.setAttribute('wrap-count', 25);
+
+      // text.setAttribute('geometry', 'primitive: plane; height: auto; width: auto');
+      // text.setAttribute('material', 'color: #FFFFFF');
 
       scene.appendChild(text);
-
     }
   });
