9724d2c50213ac7cee944fd9039f67a37cd0975c
Kevin Chen <k3vnchen@gmail.com>
2019-10-26 00:03:27 -0400

    Add box based on gaze direction

--- components/bulletin-text.js
+++ components/bulletin-text.js
@@ -12,29 +12,48 @@ AFRAME.registerComponent('bulletin-text', {
       var scene = document.querySelector('a-scene');
 
       scene.addEventListener('textPlace', this.onTextPlaced);
-
-      // var board = document.createElement('a-entity');
-      // board.setAttribute('geometry', 'primitive: cylinder; height: 3; radius: 3; open-ended: true');
-      // board.setAttribute('material', 'color: #F7B860; opacity: 0.7; side: double');
-      // board.setAttribute('position', '0 2 0');
-
-      // scene.appendChild(board);
     },
 
     bindMethods: function() {
       this.onTextPlaced = this.onTextPlaced.bind(this);
+      this.placeText = this.placeText.bind(this);
     },
 
     onTextPlaced: function() {
-      var cam = document.getElementById('cam');
       console.log('detected enter');
+      
+      var cam = document.getElementById('cam');
       var position = cam.object3D.position;
       var rotation = cam.object3D.rotation;
-      console.log(position);
-      console.log(rotation.x);
-      console.log(rotation.y);
       var x = rotation.x;
       var y = rotation.y;
+
+      console.log(position);
+      console.log(x);
+      console.log(y);
+
+      this.placeText(x, y, "Hello, World!");
+    },
+
+    placeText: function(x, y, str) {
+      var r = 3;
+      var h = 3;
+      var mid = 2;
+      var cam = 1.6;
+      var scene = document.querySelector('a-scene');
+
+      var text = document.createElement('a-entity');
+      var xpos = -r * Math.sin(y);
+      var ypos = r * Math.tan(x) + cam;
+      var zpos = -r * Math.cos(y);
+      console.log(xpos);
+      console.log(ypos);
+      console.log(zpos);
+      text.setAttribute('position', xpos.toString() + ' ' + ypos.toString() + ' ' + zpos.toString());
+      text.setAttribute('geometry', 'primitive: box; acolor: red');
+      // text.setAttribute('text', 'value: ' + str + '; width: 40; color: black');
+
+      scene.appendChild(text);
 
     }
   });
